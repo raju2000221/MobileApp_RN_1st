d6aea5073249ae136a987c4d9121a0b29dd09eae
Raju Islam <bdrajuislam246@gmail.com>
2024-03-31 04:26:12 +0600

    integrate with server add pass the user regi. data in server

--- src/Login_Signin/Register.jsx
+++ src/Login_Signin/Register.jsx
@@ -1,90 +1,116 @@
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import styles from '../../src/Style/style'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faKey, faLock, faMailBulk, faMobile, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle, faCircleXmark, faKey, faLock, faMailBulk, faMobile, faUser } from '@fortawesome/free-solid-svg-icons'
 import { useNavigation } from '@react-navigation/native'
+import axios from 'axios'
 
 const Register = () => {
     const navigation = useNavigation();
+    const [mobile, setMobile] = useState('');
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const handleRegister = () => {
+        // Create an object containing all the input values
+        const formData = {
+            mobile: mobile,
+            email: email,
+            name: name,
+            password: password,
+            confirmPassword: confirmPassword
+        };
+
+        axios.post('http://192.168.0.112:5000/register', formData)
+            .then(response => {
+                console.log('Registration successful:', response.data);
+                navigation.navigate('Login');
+            })
+            .catch(error => {
+                console.error('Registration failed:', error);
+            });
+    };
     return (
-        <View>
-            <View style={styles.logoContainer}>
-                <Image style={styles.logoRegister} source={require('../../src/assets/icon.png')} />
-            </View>
-            <View style={styles.loginContainer}>
-                <Text style={{textAlign:'center', fontSize:25}}>--- Register ---</Text>
-                <View style={styles.action}>
-                    <FontAwesomeIcon icon={faMobile} size={20} />
-                    <TextInput
-                        style={{ paddingHorizontal: 20 }}
-                        placeholder='Mobile'
-                    />
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View>
+                <View style={styles.logoContainer}>
+                    <Image style={styles.logoRegister} source={require('../../src/assets/icon.png')} />
                 </View>
-                <View style={styles.action}>
-                    <FontAwesomeIcon icon={faMailBulk} size={20} />
-                    <TextInput
-                        style={{ paddingHorizontal: 20 }}
-                        placeholder='Email'
-                    />
+                <View style={styles.loginContainer}>
+                    <Text style={{ textAlign: 'center', fontSize: 25 }}>--- Register ---</Text>
+                    <View style={styles.action}>
+                        <FontAwesomeIcon icon={faMobile} size={20} />
+                        <TextInput
+                            style={{ paddingHorizontal: 20 }}
+                            onChangeText={setMobile}
+                            placeholder='Mobile'
+                        />
+                    </View>
+                    <View style={styles.action}>
+                        <FontAwesomeIcon icon={faMailBulk} size={20} />
+                        <TextInput
+                            style={{ paddingHorizontal: 20 }}
+                            placeholder='Email'
+                            onChangeText={setEmail}
+                        />
+                    </View>
+                    <View style={styles.action}>
+                        <FontAwesomeIcon icon={faUser} size={20} />
+                        <TextInput
+                            onChangeText={setName}
+                            style={{ paddingHorizontal: 20 }}
+                            placeholder='Name'
+                        />
+
+                    </View>
+                    <View style={styles.action}>
+                        <FontAwesomeIcon icon={faLock} size={20} />
+                        <TextInput
+                            style={{ paddingHorizontal: 20 }}
+                            placeholder='Password'
+                            onChangeText={setPassword}
+                        />
+                    </View>
+                    <View style={styles.action}>
+                        <FontAwesomeIcon icon={faLock} size={20} />
+                        <TextInput
+                            style={{ paddingHorizontal: 20 }}
+                            placeholder='Confirm Password'
+                            onChangeText={setConfirmPassword}
+                        />
+                    </View>
+
+
                 </View>
-                <View style={styles.action}>
-                    <FontAwesomeIcon icon={faUser} size={20} />
-                    <TextInput
-                        style={{ paddingHorizontal: 20 }}
-                        placeholder='Name'
-                    />
-                </View>
-                <View style={styles.action}>
-                    <FontAwesomeIcon icon={faLock} size={20} />
-                    <TextInput
-                        style={{ paddingHorizontal: 20 }}
-                        placeholder='Password'
-                    />
-                </View>
-                <View style={styles.action}>
-                    <FontAwesomeIcon icon={faLock} size={20} />
-                    <TextInput
-                        style={{ paddingHorizontal: 20 }}
-                        placeholder='Confirm Password'
-                    />
-                </View>
-                <View style={
-                    {
-                        alignItems: 'flex-end',
-                        marginTop: 10
-                    }
-                }>
-                    <Text style={{ color: '#043265', fontWeight: 700 }}>Forgot password ?</Text>
+                <View style={styles.button}>
+                    <TouchableOpacity onPress={handleRegister}>
+                        <View style={{
+                            backgroundColor: '#043265',
+                            paddingHorizontal: 150,
+                            padding: 15,
+                            borderRadius: 50,
+                        }}>
+                            <Text style={{ color: '#FFFFFF', fontSize: 15, fontWeight: 700 }}> {/* Set text color to white */}
+                                Register
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
 
-            </View>
-            <View style={styles.button}>
-                <TouchableOpacity>
-                    <View style={{
-                        backgroundColor: '#043265',
-                        paddingHorizontal: 150,
-                        padding: 15,
-                        borderRadius: 50,
-                    }}>
-                        <Text style={{ color: '#FFFFFF', fontSize: 15, fontWeight: 700 }}> {/* Set text color to white */}
-                            Log in
-                        </Text>
-                    </View>
-                </TouchableOpacity>
-            </View>    
-            
-            <View style={{alignItems:'center'}}>
+                <View style={{ alignItems: 'center' }}>
                     <Text style={{ fontSize: 15, fontWeight: '500', }}>
                         Allready Have an Account ?
-                        <TouchableOpacity onPress={() => {navigation.navigate('Login')}}>
-                            <Text style={{ fontWeight: '700', color: '#043265', textDecorationLine:'underline', marginLeft:10}}>
+                        <TouchableOpacity onPress={() => { navigation.navigate('Login') }}>
+                            <Text style={{ fontWeight: '700', color: '#043265', textDecorationLine: 'underline', marginLeft: 10 }}>
                                 Login Now
                             </Text>
                         </TouchableOpacity>
                     </Text>
-                </View>      
-        </View>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
